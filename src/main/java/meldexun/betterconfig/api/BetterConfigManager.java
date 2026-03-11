14c0f60e94bce98f91fff857a7ede5d426c90afe
Meldexun <27779321+Meldexun@users.noreply.github.com>
2026-03-11 16:43:23 +0100

    Expose replacement method for ConfigManager.sync

---
+++ src/main/java/meldexun/betterconfig/api/BetterConfigManager.java
@@ -0,0 +1,11 @@
+package meldexun.betterconfig.api;
+
+import meldexun.betterconfig.ConfigManager;
+
+public class BetterConfigManager {
+
+	public static void sync(String modid) {
+		ConfigManager.sync(modid);
+	}
+
+}
