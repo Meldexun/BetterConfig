031af8d4d429ad20f812683f3120ef62e5120214
Meldexun <27779321+Meldexun@users.noreply.github.com>
2026-07-24 19:49:21 +0200

    Atomic config writing

--- src/main/java/meldexun/betterconfig/Config.java
+++ src/main/java/meldexun/betterconfig/Config.java
@@ -5,6 +5,7 @@ import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
@@ -87,30 +88,41 @@ class Config {
 	}
 
 	void save(Path file, Function<String, Type> getType) throws IOException {
-		try (ConfigWriter writer = new ConfigWriter(Files.newBufferedWriter(file))) {
-			writer.writeCommentLine("Configuration file");
-			writer.newLine();
-			for (Map.Entry<String, ConfigCategory> entry : this.categories.entrySet()) {
-				String name = entry.getKey();
-				ConfigCategory category = entry.getValue();
-				Type type = getType.apply(name);
-				BetterConfig settings = type != null ? AnnotationUtil.get(type, BetterConfig.class) : DEFAULT_SETTINGS;
-				ConfigElementMetadata metadata = type != null ? ConfigElementMetadata.create(TypeUtil.getRawType(type)) : null;
-				if (name.isEmpty()) {
-					boolean writeComments = type != null && !TypeUtil.isMap(type);
-					for (ConfigCategory.Entry entry1 : category.elements(settings, type, metadata, null)) {
-						ConfigCategory.writeEntry(writer, settings, entry1.name(), entry1.configElement(), entry1.type(), entry1.metadata(), entry1.instance(), writeComments);
+		Path temp = file.resolveSibling(".temp");
+		try {
+			try (ConfigWriter writer = new ConfigWriter(Files.newBufferedWriter(temp))) {
+				writer.writeCommentLine("Configuration file");
+				writer.newLine();
+				for (Map.Entry<String, ConfigCategory> entry : this.categories.entrySet()) {
+					String name = entry.getKey();
+					ConfigCategory category = entry.getValue();
+					Type type = getType.apply(name);
+					BetterConfig settings = type != null ? AnnotationUtil.get(type, BetterConfig.class) : DEFAULT_SETTINGS;
+					ConfigElementMetadata metadata = type != null ? ConfigElementMetadata.create(TypeUtil.getRawType(type)) : null;
+					if (name.isEmpty()) {
+						boolean writeComments = type != null && !TypeUtil.isMap(type);
+						for (ConfigCategory.Entry entry1 : category.elements(settings, type, metadata, null)) {
+							ConfigCategory.writeEntry(writer, settings, entry1.name(), entry1.configElement(), entry1.type(), entry1.metadata(), entry1.instance(), writeComments);
+							writer.newLine();
+							writer.newLine();
+							writer.newLine();
+						}
+					} else {
+						ConfigCategory.writeEntry(writer, settings, name, category, type, metadata, null, true);
 						writer.newLine();
 						writer.newLine();
 						writer.newLine();
 					}
-				} else {
-					ConfigCategory.writeEntry(writer, settings, name, category, type, metadata, null, true);
-					writer.newLine();
-					writer.newLine();
-					writer.newLine();
 				}
 			}
+			Files.move(temp, file, StandardCopyOption.ATOMIC_MOVE);
+		} catch (IOException e) {
+			try {
+				Files.delete(temp);
+			} catch (IOException e1) {
+				e.addSuppressed(e1);
+			}
+			throw e;
 		}
 	}
 
