4f528e8829d387e32add5e9969840d1f64d41088
Meldexun <27779321+Meldexun@users.noreply.github.com>
2026-07-27 10:40:48 +0200

    Add support for Vec3i, Vec3d and BlockPos

--- src/main/java/meldexun/betterconfig/asm/BetterConfigClassTransformer.java
+++ src/main/java/meldexun/betterconfig/asm/BetterConfigClassTransformer.java
@@ -14,6 +14,9 @@ import meldexun.betterconfig.api.BetterConfig;
 import meldexun.betterconfig.gui.configuration.ConfigurationGuiRegistry;
 import net.minecraft.launchwrapper.IClassTransformer;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.Vec3i;
 import net.minecraftforge.fml.common.LoaderException;
 import net.minecraftforge.fml.common.discovery.ASMDataTable;
 import net.minecraftforge.fml.common.discovery.ASMDataTable.ASMData;
@@ -35,6 +38,31 @@ public class BetterConfigClassTransformer extends HashMapClassNodeClassTransform
 
 		public static void loadBetterConfigClasses(ASMDataTable asmDataTable) {
 			TypeAdapters.register(ResourceLocation::toString, ResourceLocation::new, new ResourceLocation("unkown"), ResourceLocation.class);
+
+			TypeAdapters.register(v -> v.getX() + "," + v.getY() + "," + v.getZ(), s -> {
+				String[] a = s.split(",");
+				if (a.length != 3) throw new IllegalArgumentException();
+				return new Vec3i(
+						Integer.parseInt(a[0].trim()),
+						Integer.parseInt(a[1].trim()),
+						Integer.parseInt(a[2].trim()));
+			}, Vec3i.NULL_VECTOR, Vec3i.class);
+			TypeAdapters.register(v -> v.x + "," + v.y + "," + v.z, s -> {
+				String[] a = s.split(",");
+				if (a.length != 3) throw new IllegalArgumentException();
+				return new Vec3d(
+						Double.parseDouble(a[0].trim()),
+						Double.parseDouble(a[1].trim()),
+						Double.parseDouble(a[2].trim()));
+			}, Vec3d.ZERO, Vec3d.class);
+			TypeAdapters.register(v -> v.getX() + "," + v.getY() + "," + v.getZ(), s -> {
+				String[] a = s.split(",");
+				if (a.length != 3) throw new IllegalArgumentException();
+				return new BlockPos(
+						Integer.parseInt(a[0].trim()),
+						Integer.parseInt(a[1].trim()),
+						Integer.parseInt(a[2].trim()));
+			}, BlockPos.ORIGIN, BlockPos.class);
 
 			for (ASMData target : asmDataTable.getAll(BetterConfig.class.getName())) {
 				try {
