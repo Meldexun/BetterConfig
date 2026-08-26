5be7e6a96833d948bf1d1d5be64f3dec85291d76
Meldexun <27779321+Meldexun@users.noreply.github.com>
2026-08-26 14:53:33 +0200

    Add explanation for ConfigSyncedEvent

--- src/main/java/meldexun/betterconfig/api/ConfigSyncedEvent.java
+++ src/main/java/meldexun/betterconfig/api/ConfigSyncedEvent.java
@@ -1,7 +1,15 @@
 package meldexun.betterconfig.api;
 
 import net.minecraftforge.fml.common.eventhandler.Event;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
 
+/**
+ * Only fired for config classes annotated with {@link Sync}.<br>
+ * <br>
+ * Fired on logical client side after copying config options from a sync packet to the runtime config class.<br>
+ * <br>
+ * Sync packets are sent when {@link PlayerLoggedInEvent} is fired and when changing a config using the ingame GUI while an integrated server is running.
+ */
 public class ConfigSyncedEvent extends Event {
 
 	private final Class<?> slaveConfigClass;
