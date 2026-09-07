a3cc8e337e476ce371a03f68b805bc439992c65b
Meldexun <27779321+Meldexun@users.noreply.github.com>
2026-09-07 16:07:58 +0200

    Add javadoc for LoadEarly.Callback

--- src/main/java/meldexun/betterconfig/api/LoadEarly.java
+++ src/main/java/meldexun/betterconfig/api/LoadEarly.java
@@ -5,10 +5,24 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import net.minecraftforge.fml.client.event.ConfigChangedEvent.OnConfigChangedEvent;
+
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface LoadEarly {
 
+	/**
+	 * <p>
+	 * Normally, {@link OnConfigChangedEvent} is fired after the config is loaded initially, but when loading a config early, Forge's event bus isn't available yet. Use this callback to process your config immediately after it was loaded.
+	 * </p>
+	 * 
+	 * <pre>{@code
+	 * @LoadEarly.Callback
+	 * public static void afterEarlyLoad() {
+	 *     // ...
+	 * }
+	 * }</pre>
+	 */
 	@Retention(RetentionPolicy.RUNTIME)
 	@Target(ElementType.METHOD)
 	@interface Callback {
